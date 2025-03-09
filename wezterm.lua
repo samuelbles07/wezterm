08650375ef0ec6037750948c638a5b91da0d2a7e
Samuel Siburian <bles@192.168.1.5>
2025-03-09 07:03:52 +0700

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,148 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- config.color_scheme = 'SoftServer'
+-- config.color_scheme = 'Snazzy'
+config.color_scheme = "nordfox"
+
+-- Fonts
+-- config.font = wezterm.font("Hack Nerd Font", { weight = "Bold" })
+config.font = wezterm.font("Hack Nerd Font", { weight = 'Regular', italic = false })
+-- config.font = wezterm.font('Fira Code', { weight = 'Regular', italic = false }) 
+config.font_size = 13
+
+-- Windows
+config.window_decorations = "RESIZE"
+-- config.enable_tab_bar = false
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 50
+
+-- How many lines of scrollback you want to retain per tab
+config.scrollback_lines = 8000
+
+-- Tabs
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab_title(tab)
+	if tab.is_active then
+		return {
+			{ Background = { Color = "#c8c800" } },
+			{ Text = tab.tab_index .. ":" .. title .. " " },
+		}
+	end
+	-- return title
+	return {
+		{ Text = tab.tab_index .. ":" .. title .. " " },
+	}
+end)
+
+-- keymaps
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "CTRL|ALT",
+		key = "[",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "CTRL|ALT",
+		key = "]",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "Enter",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+}
+
+-- Set switching tab using tab number
+for i = 1, 8 do
+	-- CTRL+ALT + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
+
+return config
